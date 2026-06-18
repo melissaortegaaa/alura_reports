1ca9bcdb5da620148a26e32a6da155d35fad7426
melissaortegaaa <melissa.ortega@premexcorp.com>
2026-06-17 20:10:06 -0500

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import AccuremaxAuditoria from './AccuremaxAuditoria'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AccuremaxAuditoria />
-  </StrictMode>,
-)
